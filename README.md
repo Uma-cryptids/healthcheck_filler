c0eff5e5530c4ba0c35fc2799f9f73f62dd6783f
tetra <three.tetration.three@gmail.com>
2021-10-28 16:48:41 +0900

    prototype

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # healthcheck_filler
+
+元気で怠惰な人間のためのツール。
+特定の健康調査票に適当にそれっぽい数字を入れてくれる。
+
+## 使い方
+pythonはインストールされている前提です。
+```
+$ pip install openpyxl  # for pip
+$ conda install openpyxl # for conda
+```
+``filer.py``とおなじ階層に``healthcheck3_202109-202112.xlsx``を置く。
+(学校のホームページにある。)
+```
+$ python3 filer.py
+>>> 平均体温を入力してください。
+36.5
+Done!!
+```
+終了
---
+++ filler.py
@@ -0,0 +1,31 @@
+import openpyxl
+import random
+
+
+def btm(average):
+    tmp = random.normalvariate(average, 0.3)
+    while tmp < average - 1 or 37.5 <= tmp:
+        tmp = random.normalvariate(average, 0.3)
+    return tmp
+
+
+ave = 0.0
+while ave < 33.0 or 37.3 <= ave:
+    ave = float(input(">>> 平均体温を入力してください。"))
+
+book = openpyxl.load_workbook("healthcheck3_202109-202112.xlsx")
+for sheet_title in book.sheetnames:
+    sheet = book[sheet_title]
+    for row in [3, 19]:
+        for first_char in ["", "A"]:
+            for second_char in range(65, 91):
+                column = first_char+chr(second_char)
+                column_for_check = first_char+chr(second_char+1)
+                if column == "A":
+                    continue
+                if sheet[column+str(row)].value not in ["/", None]:
+                    sheet[column+str(row+1)].value = round(btm(ave), 1)
+                    sheet[column+str(row+2)].value = round(btm(ave), 1)
+                    sheet[column_for_check+str(row+13)].value = "✓"
+book.save("healthcheck3_202109-202112.xlsx")
+print("Done!!")
