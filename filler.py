23fcd5e8b32a328955feba4794c3f7bd60837faf
uma_cryptids <uma.i.3788@gmail.com>
2021-11-28 11:13:56 +0900

    add submit day detection

--- filler.py
+++ filler.py
@@ -16,6 +16,7 @@ while ave < 33.0 or 37.3 <= ave:
 book = openpyxl.load_workbook("healthcheck3_202109-202112.xlsx")
 for sheet_title in book.sheetnames:
     sheet = book[sheet_title]
+    flag = False
     for row in [3, 19]:
         for first_char in ["", "A"]:
             for second_char in range(65, 91):
@@ -27,5 +28,12 @@ for sheet_title in book.sheetnames:
                     sheet[column+str(row+1)].value = round(btm(ave), 1)
                     sheet[column+str(row+2)].value = round(btm(ave), 1)
                     sheet[column_for_check+str(row+13)].value = "✓"
+
+                if sheet[column+str(row)].value is not None and "提出日" in sheet[column+str(row)].value:
+                    flag = True
+                if flag:
+                    break
+            if flag:
+                break
 book.save("healthcheck3_202109-202112.xlsx")
 print("Done!!")
